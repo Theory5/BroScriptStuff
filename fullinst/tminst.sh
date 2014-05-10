15b9b785123dd31280371d2e966ca46e4b1c6424
Theory5 <Theory5@users.noreply.github.com>
2014-05-10 15:48:26 -0400

    Update tminst.sh

--- fullinst/tminst.sh
+++ fullinst/tminst.sh
@@ -8,11 +8,11 @@ apt-get -y update
 
 #install anything that may be a dependency.
 
-apt-get install -y libpcap0.8 libpcap0.8-dev libpcap-dev &>  ../tminst.log
+apt-get install -y libpcap0.8 libpcap0.8-dev libpcap-dev &>  tminst.log
 
 #install git if not already
 
-apt-get install -y git-core git &>>  ../tminst.log
+apt-get install -y git-core git &>>  tminst.log
 
 #download latest source (and pray they haven't altered the folder structure) and put into "timemachine"
 
@@ -22,7 +22,7 @@ cd timemachine
 
 echo "This install assumes that you've installed bro to the local prefix /usr/local/bro and not some other, weird one."
 
-./configure --with-pcaps=* --with-broccoli=/usr/local/bro &>> ../tminst.txt
+./configure --with-pcaps=* --with-broccoli=/usr/local/bro &>> ../tminst.log
 
 make &>>  ../tminst.log
 
@@ -51,4 +51,4 @@ echo "@load base/frameworks/tminst/" >> /usr/local/bro/share/bro/site/local.bro 
 # preface with a question/prompt?
 #apt-get -y remove git git-core &>>  ../tminst.log
 
-echo "a log file of this installation can be found here ../tminst.log"
+echo "a log file of this installation can be found here tminst.log"
