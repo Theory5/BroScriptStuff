a08d1074482deca510ccd16217bb5c97e7071496
Theory5 <Theory5@users.noreply.github.com>
2014-05-10 15:44:24 -0400

    Update tminst.sh

--- fullinst/tminst.sh
+++ fullinst/tminst.sh
@@ -8,11 +8,11 @@ apt-get -y update
 
 #install anything that may be a dependency.
 
-apt-get install -y libpcap0.8 libpcap0.8-dev libpcap-dev &> ~../tminst.log
+apt-get install -y libpcap0.8 libpcap0.8-dev libpcap-dev &>  ../tminst.log
 
 #install git if not already
 
-apt-get install -y git-core git &>> ~../tminst.log
+apt-get install -y git-core git &>>  ../tminst.log
 
 #download latest source (and pray they haven't altered the folder structure) and put into "timemachine"
 
@@ -24,15 +24,15 @@ echo "This install assumes that you've installed bro to the local prefix /usr/lo
 
 ./configure --with-pcaps=* --with-broccoli=/usr/local/bro &>> ../tminst.txt
 
-make &>> ~../tminst.log
+make &>>  ../tminst.log
 
-make install &>> ~../tminst.log
+make install &>>  ../tminst.log
 
 #ensure time machine can write to /usr/local/var/tm
 
-mkdir -p /usr/local/var/tm &>> ~../tminst.log
+mkdir -p /usr/local/var/tm &>>  ../tminst.log
 
-chmod 0666 /usr/local/var/tm &>> ~../tminst.log
+chmod 0666 /usr/local/var/tm &>>  ../tminst.log
 
 echo "Time Machine config found here: /usr/local/etc/timemachine.cfg"
 
@@ -40,15 +40,15 @@ echo "Time Machine config found here: /usr/local/etc/timemachine.cfg"
 
 cd ../tminst
 
-mkdir -p /usr/local/bro/share/bro/base/frameworks/tminst/ &>> ~../tminst.log
+mkdir -p /usr/local/bro/share/bro/base/frameworks/tminst/ &>>  ../tminst.log
 
-cp /timinst/main.bro /usr/local/bro/share/bro/base/frameworks/timinst/main.bro &>> ~../tminst.log
+cp /timinst/main.bro /usr/local/bro/share/bro/base/frameworks/timinst/main.bro &>>  ../tminst.log
 
-cp /timinst/__load__.bro /usr/local/bro/share/bro/base/frameworks/tminst/__load__.bro &>> ~../tminst.log
+cp /timinst/__load__.bro /usr/local/bro/share/bro/base/frameworks/tminst/__load__.bro &>>  ../tminst.log
 
-echo "@load base/frameworks/tminst/" >> /usr/local/bro/share/bro/site/local.bro &>> ~../tminst.log
+echo "@load base/frameworks/tminst/" >> /usr/local/bro/share/bro/site/local.bro &>>  ../tminst.log
 
 # preface with a question/prompt?
-#apt-get -y remove git git-core &>> ~../tminst.log
+#apt-get -y remove git git-core &>>  ../tminst.log
 
 echo "a log file of this installation can be found here ../tminst.log"
