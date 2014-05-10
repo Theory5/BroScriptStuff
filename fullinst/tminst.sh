04b57ead54c70ab6938c4739874fc84fb626e551
Theory5 <Theory5@users.noreply.github.com>
2014-05-10 15:43:57 -0400

    Update tminst.sh

--- fullinst/tminst.sh
+++ fullinst/tminst.sh
@@ -8,11 +8,11 @@ apt-get -y update
 
 #install anything that may be a dependency.
 
-apt-get install -y libpcap0.8 libpcap0.8-dev libpcap-dev &> ~/var/log/tminst.txt
+apt-get install -y libpcap0.8 libpcap0.8-dev libpcap-dev &> ~../tminst.log
 
 #install git if not already
 
-apt-get install -y git-core git &>> ~/var/log/tminst.txt
+apt-get install -y git-core git &>> ~../tminst.log
 
 #download latest source (and pray they haven't altered the folder structure) and put into "timemachine"
 
@@ -22,35 +22,33 @@ cd timemachine
 
 echo "This install assumes that you've installed bro to the local prefix /usr/local/bro and not some other, weird one."
 
-./configure --with-pcaps=* --with-broccoli=/usr/local/bro &>> ~/var/log/tminst.txt
+./configure --with-pcaps=* --with-broccoli=/usr/local/bro &>> ../tminst.txt
 
-make &>> ~/var/log/tminst.txt
+make &>> ~../tminst.log
 
-make install &>> ~/var/log/tminst.txt
+make install &>> ~../tminst.log
 
 #ensure time machine can write to /usr/local/var/tm
 
-mkdir -p /usr/local/var/tm &>> ~/var/log/tminst.txt
+mkdir -p /usr/local/var/tm &>> ~../tminst.log
 
-chmod 0666 /usr/local/var/tm &>> ~/var/log/tminst.txt
+chmod 0666 /usr/local/var/tm &>> ~../tminst.log
 
 echo "Time Machine config found here: /usr/local/etc/timemachine.cfg"
 
 #install attached scripts to bro for communication purposes
 
-cd ../
+cd ../tminst
 
-cd tminst
+mkdir -p /usr/local/bro/share/bro/base/frameworks/tminst/ &>> ~../tminst.log
 
-mkdir -p /usr/local/bro/share/bro/base/frameworks/tminst/ &>> ~/var/log/tminst.txt
+cp /timinst/main.bro /usr/local/bro/share/bro/base/frameworks/timinst/main.bro &>> ~../tminst.log
 
-cp /timinst/main.bro /usr/local/bro/share/bro/base/frameworks/timinst/main.bro &>> ~/var/log/tminst.txt
+cp /timinst/__load__.bro /usr/local/bro/share/bro/base/frameworks/tminst/__load__.bro &>> ~../tminst.log
 
-cp /timinst/__load__.bro /usr/local/bro/share/bro/base/frameworks/tminst/__load__.bro &>> ~/var/log/tminst.txt
-
-echo "@load base/frameworks/tminst/" >> /usr/local/bro/share/bro/site/local.bro &>> ~/var/log/tminst.txt
+echo "@load base/frameworks/tminst/" >> /usr/local/bro/share/bro/site/local.bro &>> ~../tminst.log
 
 # preface with a question/prompt?
-#apt-get -y remove git git-core &>> ~/var/log/tminst.txt
+#apt-get -y remove git git-core &>> ~../tminst.log
 
-echo "a log file of this installation can be found here /var/log/tminst.txt"
+echo "a log file of this installation can be found here ../tminst.log"
