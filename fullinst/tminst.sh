fc4b6e477d9499c728bf666df1495916657161b1
Theory5 <Theory5@users.noreply.github.com>
2014-05-10 13:37:49 -0400

    Update tminst.sh

--- fullinst/tminst.sh
+++ fullinst/tminst.sh
@@ -8,11 +8,11 @@ apt-get -y dist-upgrade
 
 #install anything that may be a dependency.
 
-apt-get install -y libpcap0.8 libpcap0.8-dev libpcap-dev > outputinst.txt
+apt-get install -y libpcap0.8 libpcap0.8-dev libpcap-dev &>> outputinst.txt
 
 #install git if not already
 
-apt-get install -y git-core git >> outputinst.txt
+apt-get install -y git-core git > outputinst.txt
 
 #download latest source (and pray they haven't altered the folder structure) and put into "timemachine"
 
@@ -22,7 +22,7 @@ cd timemachine
 
 echo "This install assumes that you've installed bro to the local prefix /usr/local/bro and not some other, weird one."
 
-./configure --with-pcaps=* --with-broccoli=/usr/local/bro &>> outputinst.txt
+./configure --with-pcaps=* --with-broccoli=/usr/local/bro > outputinst.txt
 
 make > outputinst.txt
 
