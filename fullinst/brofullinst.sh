94ecd01abd9d1cbc2f4e16f08d6c4115c7d253c7
Theory5 <Theory5@users.noreply.github.com>
2014-05-13 14:52:08 -0400

    Update brofullinst.sh

--- fullinst/brofullinst.sh
+++ fullinst/brofullinst.sh
@@ -1,4 +1,4 @@
-#This is for the complete installation of bro, all its dependencies and support to connect it to timemachine, whose install
+m#This is for the complete installation of bro, all its dependencies and support to connect it to timemachine, whose install
 #is located in the tminst.sh file
 #Created by Theory5 with a ton of help from April and Ziggy
 
