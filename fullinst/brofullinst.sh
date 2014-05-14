7f25a5e07204a1d12ec5ae776fe60473c9ad7ffc
Theory5 <Theory5@users.noreply.github.com>
2014-05-14 10:53:56 -0400

    Update brofullinst.sh

--- fullinst/brofullinst.sh
+++ fullinst/brofullinst.sh
@@ -9,10 +9,7 @@ apt-get -y upgrade
 
 #install dependencies that bro may need
 
-apt-get -y install install libpcap0.8 libpcap0.8-dev libpcap-dev autogen gcc make libxml2-dev libgnutls-dev libcurl4-gnutls-dev \
- libnl-dev build-essential autopoint xsltproc w3c-dtd-xhtml python-dev libxen-dev uuid-dev libdevmapper-dev \
-  libgnutls-dev libpciaccess-dev libxml2-dev pm-utils ebtables libcap2-bin libcap-dev \
-cmake make g++ flex bison libssl-dev python-dev swig zlib1g-dev libmagic-dev autoconf curl bind9 sendmail gawk
+apt-get -y install install libpcap0.8 libpcap0.8-dev libpcap-dev autogen gcc make libxml2-dev libgnutls-dev libcurl4-gnutls-dev libnl-dev build-essential autopoint xsltproc w3c-dtd-xhtml python-dev libxen-dev uuid-dev libdevmapper-dev libgnutls-dev libpciaccess-dev libxml2-dev pm-utils ebtables libcap2-bin libcap-dev cmake make g++ flex bison libssl-dev python-dev swig zlib1g-dev libmagic-dev autoconf curl bind9 sendmail gawk
 
 #install libgeoIP databases/packages
 
