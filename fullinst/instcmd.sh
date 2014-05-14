d3b9663d607cf65441ff7f7c865c124b4c8863b5
Theory5 <Theory5@users.noreply.github.com>
2014-05-14 12:11:14 -0400

    Update instcmd.sh

--- fullinst/instcmd.sh
+++ fullinst/instcmd.sh
@@ -1,6 +1,11 @@
 #This script will run all the other installation and configuration scripts here. This script contains a few bits that 
 #are required to run the rest. Only modify if you understand what this script does, and the ramifications of not using it
 
+#define all variables needed for entire script
+ROOT_UID=0     # Only users with $UID 0 have root privileges.
+E_NOTROOT=87   # Non-root exit error.
+
+
 # Run as root, of course.
 if [ "$UID" -ne "$ROOT_UID" ]
 then
